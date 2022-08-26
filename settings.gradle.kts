91f3d2f94c5e9c59e549fd8d842bf0ad64a60b39
Kazik Pogoda <morisil@xemantic.com>
2022-08-26 15:13:24 +0200

    generator module temporarily removed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,23 @@
 /*
- * xemantic-state - kotlin library for transforming state beans into
- * reactive event streams
- * Copyright (C) 2020  Kazimierz Pogoda
+ * xemantic-state -a Kotlin library providing hierarchical object state as reactive Flow of events
+ * Copyright (C) 2022 Kazimierz Pogoda
  *
  * This file is part of xemantic-state.
  *
- * xemantic-state  is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * xemantic-state is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
  *
- * xemantic-state is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * xemantic-state is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with xemantic-state.
+ * You should have received a copy of the GNU Lesser General Public License along with xemantic-state.
  * If not, see <https://www.gnu.org/licenses/>.
  */
 
 rootProject.name = "xemantic-state"
+
 include(
-    "xemantic-state-core",
-    "xemantic-state-generator"
+    "xemantic-state-core"
 )
